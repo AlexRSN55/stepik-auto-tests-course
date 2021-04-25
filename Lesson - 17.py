0acfda4deb6675a01a0fd7ed35eb0a77f21f194f
alexr <alexrus8455@gmail.com>
2021-04-25 16:49:03 +0600

    Add new files(17)

--- Lesson - 17.py
+++ Lesson - 17.py
@@ -3,34 +3,79 @@ import time
 import unittest
 
 
-class TestInput(unittest.TestCase):
-    link = "http://suninjuly.github.io/registration1.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+class TestRegis(unittest.TestCase):
 
-    def test_input(self):
-        self.assertNotEqual(self.browser.find_element_by_css_selector(".first_block .form-control.first")
-                         .send_keys("Alex"), " ")
+    def test_reg1(self):
 
-    def test_input2(self):
-        self.assertNotEqual(self.browser.find_element_by_css_selector('.first_block .form-control.second')
-                         .send_keys("Rusinov"), "Enter second name")
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-    def test_input3(self):
-        self.assertNotEqual(self.browser.find_element_by_css_selector('.first_block .form-control.third')
-                         .send_keys("alex@mail.su"), "Enter email")
+    # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector('.first_block .form-control.first')
+        input1.send_keys("Alex")
+        input2 = browser.find_element_by_css_selector('.first_block .form-control.second')
+        input2.send_keys("Rusinov")
+        input3 = browser.find_element_by_css_selector('.first_block .form-control.third')
+        input3.send_keys("alex@mail.su")
 
-    def push_button(self):
-        self.assertNotEqual(self.browser.find_element_by_css_selector(".button.btn").click(), "button click")
+    # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
 
-    def welcome_text(self):
-        self.assertEqual(self.browser.find_element_by_tag_name("h1"),
-                         "Congratulations! You have successfully registered!")
-        # self.assertEqual("Congratulations! You have successfully registered!")
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+        time.sleep(3)
+
+    # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual( "Congratulations! You have successfully registered!", welcome_text, 'error registration')
+
     # ожидание чтобы визуально оценить результаты прохождения скрипта
         time.sleep(10)
-    # # закрываем браузер после всех манипуляций
-        self.browser.quit()
+    # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_reg2(self):
+
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector('.first_block .form-control.first')
+        input1.send_keys("Alex")
+        input2 = browser.find_element_by_css_selector('.first_block .form-control.second')
+        input2.send_keys("Rusinov")
+        input3 = browser.find_element_by_css_selector('.first_block .form-control.third')
+        input3.send_keys("alex@mail.su")
+
+
+    # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+        time.sleep(3)
+
+    # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual( "Congratulations! You have successfully registered!", welcome_text, 'error registration')
+
+
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+    # закрываем браузер после всех манипуляций
+        browser.quit()
 
 
 if __name__ == "__main__":
